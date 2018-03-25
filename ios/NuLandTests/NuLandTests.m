c67624df405446f43b954c48b4d3db00b44524ac
Vasily Kharlamov <caesai@yandex.ru>
2018-03-25 17:11:57 +0300

    Recompiled project

--- ios/NuLandTests/NuLandTests.m
+++ ios/NuLandTests/NuLandTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface geoglobulaTests : XCTestCase
+@interface NuLandTests : XCTestCase
 
 @end
 
-@implementation geoglobulaTests
+@implementation NuLandTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
